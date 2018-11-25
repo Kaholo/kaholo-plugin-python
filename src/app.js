c1a17f335a8c2e977f7c2de8e3fe2120df02dfc8
kadosh1000 <kadosh10001@gmail.com>
2018-11-25 13:33:28 +0200

    * fix plugin structure

--- src/app.js
+++ src/app.js
@@ -24,27 +24,6 @@ function runScript(action){
 	return deferred.promise;
 }
 
-var functions = {
+module.exports = {
     RUN_SCRIPT: runScript,
 };
-
-function main(argv) {
-	if (argv.length < 3) {
-		console.log('{err: "not enough parameters"}');
-		// Invalid Argument
-		// Either an unknown option was specified, or an option requiring a value was provided without a value.
-		process.exit(9);
-	}
-	var action = JSON.parse(argv[2]);
-	functions[action.method.name](action).then(function(res) {
-		console.log(res);
-		process.exit(0); // Success
-	}, function(err) {
-		console.log("an error occured", err);
-        // Uncaught Fatal Exception
-		// There was an uncaught exception, and it was not handled by a domain or an 'uncaughtException' event handler.
-		process.exit(1); // Failure
-	});
-}
-
-main(process.argv);
